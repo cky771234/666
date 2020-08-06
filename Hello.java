a63c0291bf0cffbb888b5ff28f2868939dcf8295
cky771234 <cky771234@gmail.com>
2020-08-06 19:58:13 +0800

    2

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class HelloEclipse {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String regex = "[A-Z][12][0-9]{9}";
+		String regex = "[A-Z][12][0-9]{12}";
 		while(true) {
 			String id = sc.next();
 			if(id.matches(regex)) {
