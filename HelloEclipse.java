e566766f04a96704126e76a36ed0edd70b0a2435
cky771234 <cky771234@gmail.com>
2020-08-06 16:59:15 +0800

    init commit

---
+++ HelloEclipse.java
@@ -0,0 +1,18 @@
+package xxx;
+
+import java.util.*;
+
+public class HelloEclipse {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String regex = "[A-Z][12][0-9]{8}";
+		while(true) {
+			String id = sc.next();
+			if(id.matches(regex)) {
+				System.out.println(":)");
+			}else {
+				System.out.println(":(");
+			}
+		}
+	}
+}
